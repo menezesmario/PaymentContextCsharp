8db327fb38a5bd217e44d54bed891b245b492f22
menezesmario <mario082@gmail.com>
2021-06-09 17:26:15 -0300

    Value Objects

--- PaymentContext.Domain/Entities/BilletPayment.cs
+++ PaymentContext.Domain/Entities/BilletPayment.cs
@@ -1,11 +1,12 @@
 using System;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Domain.Entities
 {
     
     public class BilletPayment : Payment
     {
-        public BilletPayment(string barCode, string billetNumber,  DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, string document, string address, string email) : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
+        public BilletPayment(string barCode, string billetNumber,  DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email) : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
         {
             BarCode = barCode;
             BilletNumber = billetNumber;
